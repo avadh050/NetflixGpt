4b469e46b37f096fef5dfaad63505ed69d52a7dd
avadh <avadhraiyani050@gmail.com>
2024-08-28 12:14:14 +0530

    create sing in / sing up from

---
+++ NetflixGPT/src/components/Login.jsx
@@ -0,0 +1,70 @@
+import React,{useState} from 'react'
+import Header from './Header'
+
+const Login = () => {
+    const [isSingIn, setIsSingIn] = useState(true)
+    const toggleSingup = ()=>{
+        setIsSingIn(!isSingIn)
+    }
+  return (
+    <>
+    <Header/>
+        <div className='absolute'>
+            <img src="https://assets.nflxext.com/ffe/siteui/vlv3/36a4db5b-dec2-458a-a1c0-662fa60e7473/1115a02b-3062-4dcc-aae0-94028a0dcdff/IN-en-20240820-TRIFECTA-perspective_WEB_eeff8a6e-0384-4791-a703-31368aeac39f_large.jpg" alt="BG-IMG" />
+        </div>
+        <form className='absolute w-4/12  p-10  bg-black/80 my-36 mx-auto right-0 left-0 text-white rounded-md '>
+
+            <h1 className='text-2xl font-bold py-4'>{isSingIn ? 'Sing In' : 'Sind Up'}</h1>
+
+                {
+                    !isSingIn && (
+                        
+                <input 
+                className='p-2 my-4 w-full bg-slate-800 rounded border-gray-400 placeholder:text-gray-300'
+                type="text" 
+                placeholder="Enter your Name" />
+                    ) 
+                }
+
+                <input 
+                    className='p-2 my-4 w-full bg-slate-800 rounded border-gray-400 placeholder:text-gray-300'
+                    type="text" 
+                    placeholder="Enter your email" />
+                <input 
+                    type="password" 
+                    className='p-2 my-4 w-full  bg-slate-800 rounded border-gray-400'
+                    placeholder="Enter your Password" />
+                <button 
+                    className='p-2 my-4 bg-red-600 w-full rounded'
+                    type="submit">
+                       {isSingIn ? 'Sing In' : 'Sind Up'}
+                </button>
+                {
+                    isSingIn && (
+                        <p>New to Netflix ? 
+                    <span 
+                        onClick={toggleSingup}
+                            className='cursor-pointer font-semibold px-1 hover:underline'>
+                                Sing Up Now.
+                    </span>
+                </p>
+                    )
+                }
+                {
+                    !isSingIn && (
+                        <p>Already  have an account ? 
+
+                    <span 
+                        onClick={toggleSingup}
+                            className='cursor-pointer font-semibold px-1 hover:underline'>
+                                Sing In Now.
+                    </span>
+                </p>
+                    )
+                }
+            </form>
+        </>
+  )
+}
+
+export default Login
